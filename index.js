04eb488204349a008186f0c48af811aa5e49e092
Gajendra-Rao <gajendrapawar69@gmail.com>
2024-08-24 23:04:43 +0530

    fixed

--- index.js
+++ index.js
@@ -19,22 +19,22 @@ async function checkWeather(city) {
         document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+ "℃";
         document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
         document.querySelector(".wind").innerHTML=data.wind.speed+ "km/h";
-        if(data.weather[0].main="Clouds"){
+        if(data.weather[0].main=="Clouds"){
             weatherIcon.src="./images/clouds.png";    } 
         else
-        if(data.weather[0].main="Clear"){
+        if(data.weather[0].main=="Clear"){
             weatherIcon.src="./images/clear.png";    } 
         else
-        if(data.weather[0].main="Drizzle"){
+        if(data.weather[0].main=="Drizzle"){
             weatherIcon.src="./images/drizzle.png";    } 
         else
-        if(data.weather[0].main="Rain"){
+        if(data.weather[0].main=="Rain"){
                 weatherIcon.src="./images/rain.png";    } 
         else
-         if(data.weather[0].main="Snow"){
+         if(data.weather[0].main=="Snow"){
                     weatherIcon.src="./images/snow.png";    } 
         else
-        if(data.weather[0].main="Mist"){
+        if(data.weather[0].main=="Mist"){
                         weatherIcon.src="./images/mist.png";    } 
         console.log(data);
     }
